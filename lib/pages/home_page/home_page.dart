a6ab16a7aec84d774f21368c84c8f7c93a5841cb
prayogidcp <prayogidwicahyoputra@gmail.com>
2023-09-23 03:02:19 +0700

    add UI homepage

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -4,6 +4,7 @@ import 'package:absensi_online/services/firebase_services.dart';
 import 'package:absensi_online/utils/constant.dart';
 import 'package:absensi_online/widgets/custom_button.dart';
 import 'package:absensi_online/widgets/custom_notification.dart';
+import 'package:absensi_online/widgets/transition_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -18,37 +19,166 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-        actions: const [],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
+      // backgroundColor: Colors,
+      body: SafeArea(
+        child: SingleChildScrollView(
           child: Column(
             children: [
-              CustomButton1(
-                  btnName: "ERROR",
-                  onPress: () {
-                    customNotification(context,
-                        isError: true, message: "Message");
-                  }),
-              CustomButton1(
-                  btnName: "TIDAK",
-                  onPress: () {
-                    customNotification(context, message: "Message");
-                  }),
-              CustomButton1(
-                  btnName: "SUCCESS",
-                  onPress: () {
-                    customNotification(context,
-                        isSuccess: true, message: "Message");
-                  }),
-              CustomButton1(
-                  btnName: "Logout",
-                  onPress: () {
-                    FirebaseServices(FirebaseAuth.instance).signOut(context);
-                  })
+              Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(right: 50),
+                    height: MediaQuery.of(context).size.height / 2 - 50,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: colorBlue,
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(right: 50),
+                    height: MediaQuery.of(context).size.height / 3,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: colorDarkBlue,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.elliptical(300, 300),
+                            bottomRight: Radius.elliptical(
+                                MediaQuery.of(context).size.width * 2,
+                                MediaQuery.of(context).size.height))),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 45,
+                              height: 45,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.red,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10.0,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Jacob Hones",
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700)),
+                                Text("Jabatan",
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        color: Color(0xffF4F4F4),
+                                        fontWeight: FontWeight.w400)),
+                              ],
+                            ),
+                            Spacer(),
+                            IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.exit_to_app,
+                                  color: Colors.white,
+                                ))
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.all(20),
+                          height: 250,
+                          width: double.infinity,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Live Atendance",
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w700),
+                              ),
+                              Text(
+                                "09:41 AM",
+                                style: TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xff426689)),
+                              ),
+                              Text(
+                                "Mon, 18 April 2023",
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xff5D5D5D)),
+                              ),
+                              Divider(),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                "Office Hours",
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color(0xff5D5D5D)),
+                              ),
+                              Text(
+                                "08:00 AM - 05:00 PM",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width / 3 +
+                                            10,
+                                    child: CustomButton1(
+                                        btnName: "Check In", onPress: () {}),
+                                  ),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width / 3 +
+                                            10,
+                                    child: CustomButton1(
+                                        btnName: "Check Out", onPress: () {}),
+                                  ),
+                                ],
+                              ),
+                              Divider()
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
